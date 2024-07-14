015fc8a44d43058b5d39b1d87ddf09458ccfb46a
Nicholas Hellmers <nihe1661@colorado.edu>
2024-07-14 12:34:53 -0600

    Tested cli library

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Resume-AI
 Uses GPT api to build tailored resumes.
+
+
+## Installation
+
+- This application requires Python 3.6 or older, Selenium, BeautifulSoup, and urllib in order to run.
--- main.py
+++ main.py
@@ -2,26 +2,57 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
+import os
+
+from urllib.parse import urlparse
+
 import argparse
+
+from dotenv import load_dotenv
 
 import time
 import sys
 
+def scrape_profile(url, username, password):
+    pass
+
 def main():
+    # Load the .env file
+    load_dotenv()
+
+    # Get the username and password from the .env file
+    username = os.environ.get("USERNAME")
+    password = os.environ.get("PASSWORD")
+
+    # Check if the username and password are not None or empty, if so print an error message and exit the program
+    if username == None or password == None or username == "" or password == "":
+        print("Please provide a username and password in the .env file")
+        sys.exit(1)
+    else:
+        print(f"Username and password found in .env file with username: {username} and password: {password}")
 
     parser = argparse.ArgumentParser(
-                    prog='LinkedIn Scraper',
-                    description='This program takes in a LinkedIn Profile URL and scrapes the profile\'s information',
-                    epilog='The format of the URL should be: lparse <PROFILE_URL>')
+                    prog='resume-bud',
+                    description='This program has a few functions that can help you with your resume based on infomation you provide.',
+                    epilog='Text at the bottom of help')
     
-    parser.add_argument('URL',
-                        metavar='URL',
-                        type=str,
-                        help='The URL of the LinkedIn Profile to scrape')
+    subparsers = parser.add_subparsers()
 
-    args = parser.parse_args()
-    print(args.URL)
+    # Create a subparser for the ScrapeProfile function
+    scrape_profile_parser = subparsers.add_parser("ScrapeProfile",
+                                                  help="Scrape a LinkedIn profile by giving the URL")
     
+    # If the function is ScrapeProfile, expect a url argument to be passed with it
+    scrape_profile_parser.add_argument("url", 
+                                       help="The URL of the LinkedIn profile you want to scrape", 
+                                       nargs=1)
+    
+    args = parser.parse_args()
+
+    print(args.url)
+
+    
+                        
 
 
 if __name__ == "__main__":
